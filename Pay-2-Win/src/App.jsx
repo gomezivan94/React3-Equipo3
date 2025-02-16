b614489999237dd1a7eebe2db63c9136d3bd7275
gomezivan94 <gi.gomezivan@gmail.com>
2025-02-16 20:07:01 -0300

    Elimino linea de mas

--- Pay-2-Win/src/App.jsx
+++ Pay-2-Win/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import React from 'react';
 import Footer from './components/Footer/Footer';
-import Games from './components/Games/Games';
 import Home from './components/pages/Home/Home';
 
 

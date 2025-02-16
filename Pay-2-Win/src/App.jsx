63bf7d64501a000e9427079ca913990e00f9c2a2
Matthias291084 <amromano34@gmail.com>
2025-02-16 20:24:01 -0300

    Agregue efecto a navbar

--- Pay-2-Win/src/App.jsx
+++ Pay-2-Win/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Footer from './components/Footer/Footer';
 import Games from './components/Games/Games';
 import Home from './components/pages/Home/Home';
-
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   
@@ -12,7 +12,7 @@ function App() {
     <>
     <Games 
     />
-    
+    <Navbar/>
     
     <Home/>
     

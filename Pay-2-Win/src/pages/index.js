db1dd298ca9457bc823a4c33951a9d9c30386702
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-03-06 18:26:30 -0300

    elimino pagina de registro ya que uso modal

--- Pay-2-Win/src/pages/index.js
+++ Pay-2-Win/src/pages/index.js
@@ -2,7 +2,6 @@ export { default as About } from './About/About';
 export { default as Admin } from './Admin/Admin';
 export { default as Contact } from './Contact/Contact';
 export { default as Error404 } from './Error404/Error404';
-export { default as Login } from './Login/Login';
 export { default as Register } from './Register/Register';
 export {default as JuegoDetalle} from './JuegoDetalle/JuegoDetalle';
 export {default as Nosotros} from "./Nosotros/Nosotros";

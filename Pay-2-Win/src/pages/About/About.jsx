d69c347c550d88d17944707d8f7e16f76a856dfa
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-03-25 21:56:24 -0300

    Modifico About para hacerlo responsive

--- Pay-2-Win/src/pages/About/About.jsx
+++ Pay-2-Win/src/pages/About/About.jsx
@@ -7,16 +7,16 @@ function About() {
     <div className="about">
       <div className='container my-5'>
         <div className='row'>
-          <div className='col-lg-6 d-flex justify-content-center d-none d-lg-flex'>
-            <img src={Nosotros} alt="Nosotros" />
+          <div className='col-12 col-lg-6 d-flex justify-content-center'>
+            <img src={Nosotros} alt="Nosotros" className="img-fluid d-block mx-auto" />
           </div>
           
-          <div className='col-lg-6 d-flex flex-column align-items-center justify-content-center'>
+          <div className='col-12 col-lg-6 d-flex flex-column align-items-center justify-content-center'>
             <h1 className=' Titulo text-light'> Somos mejores que Steam</h1>
             
             <h4  className=' Parrafo text-light'>Si estas pensando en jugar, estas pensando en nosotros</h4>
             <Link to="/nosotros">
-              <button className='boton-azul btn btn-primary  btn-lg' href="/Nosotros">Conocenos mejor</button>
+              <button className='boton-about btn btn-secondary  btn-lg' href="/Nosotros">Conocenos mejor</button>
             </Link>
           </div>
         </div>

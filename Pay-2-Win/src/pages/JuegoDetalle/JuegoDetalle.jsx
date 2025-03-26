34279366668ce0dfbec5bfc9b1e4ccdd5597292d
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-03-25 22:06:34 -0300

    Agrego media queries para que se vea estetico en disp medianos y pequeños

--- Pay-2-Win/src/pages/JuegoDetalle/JuegoDetalle.jsx
+++ Pay-2-Win/src/pages/JuegoDetalle/JuegoDetalle.jsx
@@ -1,38 +1,47 @@
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
-
+import './JuegoDetalle.css'
 
 function JuegoDetalle() {
-    const { id } = useParams();
-    const juegos = JSON.parse(localStorage.getItem('juegos')) || [];
-    const juego = juegos.find((j) => j.id.toString() === id);
-  
-    if (!juego) {
-      return <h1>Juego no encontrado</h1>;
-    }
-  
-    return (
-      <>
-        <h1 className='text-white mt-1'>{juego.Title}</h1>
-        
-        <div className='px-2 mt-3 row-6 d-flex align-items-center justify-content-center mb-2'>
-        <img className='' src={juego.Header} alt="Portada" width={'75%'} height={'60%'}/>
-          </div>
-        <div className='text-white d-flex align-items-center justify-content-around'>
-          
-          <div className='d-flex flex-column align-items-start'>
-          <video alt={juego.Title} style={{ width: '60vw', height: '100%'}} autoPlay muted controls loop>
-              <source src={juego.Trailer} />
-            </video>
-            <p className=''>{juego.Description}</p>
-            <p>Categoría: {juego.Type}</p>
-            <p>Precio: ${juego.Precio}</p>
-            <Link className='btn btn-primary' to="*">Comprar</Link>
-          </div>
-        </div>
-        <Footer/>
-      </>
-    );
+  const { id } = useParams();
+  const juegos = JSON.parse(localStorage.getItem('juegos')) || [];
+  const juego = juegos.find((j) => j.id.toString() === id);
+
+  if (!juego) {
+    return <h1>Juego no encontrado</h1>;
   }
-  
-  export default JuegoDetalle;
+
+  return (
+    <>
+      <h1 className='text-white mt-1'>{juego.Title}</h1>
+
+      <div className='px-2 mt-3 row-6 d-flex align-items-center justify-content-center mb-2'>
+        <img className='' src={juego.Header} alt="Portada" width={'75%'} height={'60%'}/>
+      </div>
+
+      <div className='text-white d-flex align-items-center justify-content-around'>
+        <div className='d-flex flex-column align-items-center'>
+          <video 
+            alt={juego.Title} 
+            style={{ width: '100%', height: 'auto' }} 
+            autoPlay 
+            muted 
+            controls 
+            loop
+            className="video-responsive"
+          >
+            <source src={juego.Trailer} />
+          </video>
+          <p>{juego.Description}</p>
+          <p>Categoría: {juego.Type}</p>
+          <p>Precio: ${juego.Precio}</p>
+          <Link className='btn btn-primary' to="*">Comprar</Link>
+        </div>
+      </div>
+      
+      <Footer />
+    </>
+  );
+}
+
+export default JuegoDetalle;

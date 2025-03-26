75442a03fe6818e0fd2efdc0e5f41394963e9dd3
Natividad Rodriguez <natividad.m1990@gmail.com>
2025-03-25 21:34:07 -0300

    Modifico pagina 404

--- Pay-2-Win/src/components/Error/Error.jsx
+++ Pay-2-Win/src/components/Error/Error.jsx
@@ -1,9 +1,41 @@
 import error from '../assets/img/error404.png'
+import './Error.css'
+import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { JuegosContext } from '../../context/JuegosContext';  
+import Destacado from '../../components/Destacado/Destacado';
+import { useContext } from "react";
 
 function Error() {
+  const { juegos } = useContext(JuegosContext);
+  const juegosDestacados = juegos.filter((juego) => juego.Destacado === true);
+
   return (
-    <img className='errorimg' src={error}/>
-  )
+    <div>
+      
+      <div>
+        <Destacado juegosDestacados={juegosDestacados} />
+      </div>
+  
+      
+      <div className="error-card-container">
+        <Card className="text-center container errorcard">
+          <Card.Body>
+            <div>
+              <img className="errorimg" src={error} alt="Error 404" />
+            </div>
+            <Card.Title>Ups! parece que tuvimos un problema</Card.Title>
+            <Card.Text>
+              Por favor sigue disfrutando de nuestra plataforma mientras lo solucionamos.
+            </Card.Text>
+            <Link to="/">
+              <Button variant="secondary">Volver a Inicio</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Error
--- Pay-2-Win/src/pages/Nosotros/Nosotros.jsx
+++ Pay-2-Win/src/pages/Nosotros/Nosotros.jsx
@@ -8,36 +8,6 @@ import Santi from "../AssetsPage/Santi.jpg";
 import Matias from "../AssetsPage/Matias.jpg";
 function Nosotros() {
   return (
-<<<<<<< HEAD
- <div class="tarjeta">
-  <div className="card-body">
-  <img src={Lautaro} class="card-img-top"  height={400} width={200}  alt="..."></img>
-  <p>Me llamo Lautaro Borges Licciardi, tengo 22 años, estudié en la Escuela Normal y ahora estoy masterizando mis habilidades de programación. Me gusta jugar juegos, ir al gimnasio y hacer videos para las redes.</p>
-  </div>
-  <div className="card-body">
-  <img src={Naty} class="card-img-top" height={400} width={200} alt="..."></img>
-  <p>Me llamo Natividad, tengo 34 años, soy Tecnica de Laboratorio, trabajo en una clinica, me gusta viajar, hacer trekking, el contacto con la naturaleza, amante del café, el chocolate y el vino, de vez en cuando suelo participar en talleres al respecto.</p>
-  </div>
-  <div className="card-body">
-  <img src={maxi} class="card-img-top" height={400} width={200} alt="..."></img>
-  <p>Me llamo Maximiliano Gamboa tengo 21 años estudio en la UNSTA (desarrollo de software),en la aticana curso de inglés ,me gusta jugar juego en la compu ,caminar y ir al gimnasio</p>
-  </div>
-  <div className="card-body">
-  <img src={Santi} class="card-img-top" height={400} width={200} alt="..."></img>
-  <p>Santiago Nieva Glembocki, 22 años.
-  Actualmente, por cursar 5to año de Ing. En sistemas de Información.Me gusta hacer deporte, disfrutar con amigos y aprender cosas nuevas.</p>
- </div>
-  <div className="card-body">
- 
-  <img src={Ivan} class=" card-img-top" height={400} width={200} alt="..."></img>
-  <p>Iván Gomez, 31 años, Soporte IT / Help Desk. Disfruto aprender tecnologias nuevas y jugar videojuegos</p>
-  </div>
-  <div className="card-body">
-  <img src={Matias} class="card-img-top" height={400} width={200} alt="..."></img>
-  <p className='parrafo'>Me llamo Matias y tengo 40 años. Soy profesor de Ingles y dibujante. Trabajo en soporte tecnico de Personal, Cablevision y Fibertel hace 12 años. Amo la tecnologia y en especial, los videojuegos.</p>
-  </div>
- </div>
-=======
     <div class="row row-cols-1 row-cols-md-3 g-4">
   <div class="col">
     <div class="card nosotros-card bg-dark">
@@ -97,7 +67,6 @@ Actualmente, por cursar 5to año de Ing. En sistemas de Información.Me gusta ha
   </div>
 </div>
 
->>>>>>> fbdf0a65f7038d3a4b324a2d0f006ea652a9fa3c
   )
 }
 
